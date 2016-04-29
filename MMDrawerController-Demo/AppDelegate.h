d95d192f4d79d134d7815272037684af55760108
yezhongxiang <756374893@qq.com>
2016-04-29 11:28:25 +0800

    Initial Commit

---
+++ MMDrawerController-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMDrawerController-Demo
+//
+//  Created by YeZhongxiang on 16/4/29.
+//  Copyright © 2016年 YeZhongxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMDrawerController-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MMDrawerController-Demo
+//
+//  Created by YeZhongxiang on 16/4/29.
+//  Copyright © 2016年 YeZhongxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
